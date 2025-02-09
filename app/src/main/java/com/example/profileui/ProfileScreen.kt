88578d990dc04710836bdb99e6887c38466da1e7
KellyR <kellyruiz.dev@gmail.com>
2025-02-08 21:14:19 -0800

    initial commit

---
+++ app/src/main/java/com/example/profileui/ProfileScreen.kt
@@ -0,0 +1,171 @@
+package com.example.profileui
+
+import android.graphics.drawable.Drawable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ProfileScreen(){
+
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(name = "krramones", modifier = Modifier.padding(10.dp))
+        Spacer(modifier = Modifier.height(4.dp))
+        ProfileSection()
+
+    }
+
+}
+
+@Composable
+fun TopBar(
+    name : String,
+    modifier: Modifier = Modifier
+){
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier.fillMaxWidth()
+    ){
+        Icon(
+            imageVector = Icons.Default.ArrowBack,
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+
+        Text(
+            text = name,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+
+        Icon(
+            painter = painterResource(id = R.drawable.ic_bell),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+        Icon(
+            painter = painterResource(id = R.drawable.ic_dotmenu),
+            contentDescription = "Back",
+            tint = Color.Black,
+            modifier = Modifier.size(24.dp)
+        )
+    }
+
+}
+
+
+@Composable
+fun ProfileSection(
+    modifier: Modifier = Modifier
+){
+    Column (modifier = modifier.fillMaxWidth()){
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+                .padding(horizontal = 20.dp)
+        ){
+            RoundImage(image = painterResource(id = R.drawable.profile_user),
+                modifier = Modifier.size(100.dp)
+                    .weight(3f)
+            )
+            Spacer(modifier = Modifier.width(16.dp))
+            StatSection(modifier = Modifier.weight(7f))
+        }
+    }
+}
+
+
+@Composable
+fun RoundImage(
+    image : Painter,
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = image,
+        contentDescription = "profile",
+        modifier = modifier
+            .aspectRatio(1f, matchHeightConstraintsFirst = true)
+            .border(
+                width = 1.dp,
+                color = Color.LightGray,
+                shape = CircleShape
+            )
+            .padding(3.dp)
+            .clip(CircleShape)
+    )
+}
+
+@Composable
+fun StatSection(modifier: Modifier = Modifier){
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceAround,
+        modifier = modifier
+    ){
+        ProfileStat(numberText = "32", text = "Post")
+        ProfileStat(numberText = "100K", text = "Followers")
+        ProfileStat(numberText = "35k", text = "Following")
+
+
+    }
+}
+
+@Composable
+fun ProfileStat(
+    numberText: String,
+    text : String,
+    modifier: Modifier = Modifier
+){
+    Column (
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ){
+        Text(
+            text = numberText,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+        Spacer(modifier = Modifier.height(4.dp))
+        Text(
+            text = text,
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp
+        )
+    }
+}
